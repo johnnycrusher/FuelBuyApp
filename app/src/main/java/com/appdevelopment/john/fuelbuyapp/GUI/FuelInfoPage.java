d9087f58e1aa7d0f8eeac34bf325cfc8efbe7d56
John Huynh <john.huynh1996@gmail.com>
2018-07-09 04:23:38 +1000

    Added: Passing Info from HomePage to FuelInfoPage

--- app/src/main/java/com/appdevelopment/john/fuelbuyapp/GUI/FuelInfoPage.java
+++ app/src/main/java/com/appdevelopment/john/fuelbuyapp/GUI/FuelInfoPage.java
@@ -1,8 +1,13 @@
 package com.appdevelopment.john.fuelbuyapp.GUI;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.appdevelopment.john.fuelbuyapp.NetworkRequest.DataAccess;
 import com.appdevelopment.john.fuelbuyapp.R;
+import android.util.Log;
 
 
 public class FuelInfoPage extends AppCompatActivity {
@@ -11,5 +16,21 @@ public class FuelInfoPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_info_page);
+        Intent intent = getIntent();
+        int postcode = intent.getIntExtra(HomePage.EXTRA_NUMBER,0);
+        String fuelType = intent.getStringExtra(HomePage.EXTRA_TEXT);
+
+//        Log.i("Postcode",Integer.toString(postcode));
+//        Log.i("FuelType", fuelType);
+
     }
+
+//    private class executeDataAccess extends AsyncTask<Void,Void,Void>{
+//        String [][] tableElements;
+//        @Override
+//        protected Void doInBackground(Void... arg0){
+//            DataAccess connection = new DataAccess();
+//
+//        }
+//    }
 }
--- app/src/main/java/com/appdevelopment/john/fuelbuyapp/GUI/HomePage.java
+++ app/src/main/java/com/appdevelopment/john/fuelbuyapp/GUI/HomePage.java
@@ -1,14 +1,20 @@
 package com.appdevelopment.john.fuelbuyapp.GUI;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.appdevelopment.john.fuelbuyapp.R;
 
 public class HomePage extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "com.appdevelopment.john.fuelbuyapp.EXTRA_STRING";
+    public static final String EXTRA_NUMBER = "com.appdevelopment.john.fuelbuyapp.EXTRA_NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,17 @@ public class HomePage extends AppCompatActivity {
         });
     }
     public void openFuelInfoPage(){
+        TextInputEditText postcodeInput = (TextInputEditText) findViewById(R.id.postcode);
+        int postcodeInteger = Integer.parseInt(postcodeInput.getText().toString());
+
+        Spinner fuelSpinner = (Spinner) findViewById(R.id.fuelTypeSpinner);
+        String fuelTypeString = fuelSpinner.getSelectedItem().toString();
+//        Log.i("Postcode:",Integer.toString(postcodeInteger));
+//        Log.i("Fuel Type:", fuelTypeString);
+
         Intent intent = new Intent(this, FuelInfoPage.class);
+        intent.putExtra(EXTRA_NUMBER, postcodeInteger);
+        intent.putExtra(EXTRA_TEXT,fuelTypeString);
         startActivity(intent);
     }
 }
